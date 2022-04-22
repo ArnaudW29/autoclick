3f01495a6be51351f65c1a57c572dd02c389211d
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2022-04-22 21:31:59 +0200

    Add files via upload

--- spam.py
+++ spam.py
@@ -6,15 +6,27 @@ from pynput.keyboard import Listener, KeyCode
 TOGGLE_KEY = KeyCode(char="s")
 clicking = False
 
-print("launching")
+cps = input("number between 1 and 5")
 
-time.sleep(3)
+
+def test(input_chk):
+    global cps
+    if input_chk.strip().isdigit() and int(input_chk) <= 5:
+        print("launching in 3 sec")
+    else:
+        cps = input("not a number ! or too big")
+        test(cps)
+
+
+test(cps)
+cps = int(cps)
+time.sleep(1), print("launching in 2 sec"), time.sleep(1), print("launching in 1 sec"), time.sleep(1), print("launched")
 
 
 def click():
     while True:
         if clicking:
-            pyautogui.click(clicks=3)
+            pyautogui.click(clicks=cps)
         time.sleep(0.001)
 
 
@@ -26,7 +38,6 @@ def toggle_event(key):
 
 click_thread = threading.Thread(target=click)
 click_thread.start()
-
 
 with Listener(on_press=toggle_event) as listener:
     listener.join()
